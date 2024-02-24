f3bfce5fd9b95e81ebc7ff3a7a9d9f01419a0540
l.morenof.2021 <l.morenof.2021@alumnos.urjc.es>
2024-02-24 18:30:21 +0100

    l51

--- P2/L5/l51.js
+++ P2/L5/l51.js
@@ -1,33 +1,36 @@
 console.log("Ejecutando js...")
-//--leer el display del boton 1
+//-- Leer el display del botón 1
 const dsp1 = document.getElementById('disp1')
 
-//-- Leer el boton identificado como bt1
-//--el id del button en html es bt1 asi q lo ponemos
+//-- Leer el botón identificado como bt1
 const btn1 = document.getElementById('bt1')
 
-//--leer el boton 2 identificado como bt2
+//-- Leer el botón indentificado como bt2
 const btn2 = document.getElementById('bt2')
 
 //-- Configurar el manejador para el evento de
-//-- pulsación de botón
+//-- pulsación de botón 1
 btn1.onclick = () => {
-  //console.log("Click sobre el botón 1...")
-  //dsp1.innerHTML = "Click sobre el botón 1..."
-    dsp1.innerHTML += " 1"
-    //dsp1.style.backgroundColor = "yellow"
+  //console.log("Click sobre el botón 1 ... ")
 
-        if (dsp1.style.backgroundColor == "") {
-            dsp1.style.backgroundColor = "yellow";
-        }
-        else {
-            dsp1.style.backgroundColor = "";
-        }
+  //dsp1.innerHTML = "Click sobre el botón 1 ... "
+    dsp1.innerHTML += " 1" 
 
+  //dsp1.style.backgroundColor = "yellow"
+
+    if (dsp1.style.backgroundColor == "") {
+        dsp1.style.backgroundColor = "yellow";
+    }
+    //-- Si ya tiene color se lo quitamos
+    else {
+        dsp1.style.backgroundColor = "";
+    }  
 
 }
 
 btn2.onclick = () => {
-    dsp1.innerHTML += " 2"
-    dsp1.style.backgroundColor = "red"
+
+   dsp1.innerHTML += " 2"  
+
+   dsp1.style.backgroundColor = "red"
 }
