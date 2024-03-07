41ef3beb053a702dfc0643125af3846dd8215c53
l.morenof.2021 <l.morenof.2021@alumnos.urjc.es>
2024-03-07 12:52:48 +0100

    random

--- P2/botones.js
+++ P2/botones.js
@@ -1,27 +1,51 @@
+
+
 //-- Elementos de la gui
 const gui = {
   clave1 : document.getElementById("clave1"), 
   clave2 : document.getElementById("clave2"), 
   clave3 : document.getElementById("clave3"),  
   clave4 : document.getElementById("clave4"), 
+  // claves: document.getElementsByClassName("claves"),
   display : document.getElementById("display"),
   start : document.getElementById("start"),
   stop : document.getElementById("stop"),
-  reset : document.getElementById("reset")
+  reset : document.getElementById("reset"),
+  botones: document.getElementsByClassName("numero")
 
-}
-
+};
+console.log(gui)
 console.log("Ejecutando JS...");
-
-const min = 0
 const max = 9
 
-function clavealeatoria(min,max) {
-  caleatoria1 = Math.random(min,max);
-  document.getElementById("clave1").innerHTML = caleatoria1
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
+// clave1 = console.log(getRandomInt(max));
+// clave2 = console.log(getRandomInt(max));
+// clave3 = console.log(getRandomInt(max));
+// clave4 = console.log(getRandomInt(max));
+
+const clave1Value = getRandomInt(max);
+const clave2Value = getRandomInt(max);
+const clave3Value = getRandomInt(max);
+const clave4Value = getRandomInt(max);
+
+gui.clave1.innerHTML = clave1Value;
+gui.clave2.innerHTML = clave2Value;
+gui.clave3.innerHTML = clave3Value;
+gui.clave4.innerHTML = clave4Value;
+
+
+// caleatoria = ["clave1","clave2","clave3","clave4"];
+// console.log(caleatoria);
+
+
+gui.boton1.onclick = () => {
+  console.log("click 1")
+  gui.clave1.innerHTML = "hola";
+}
 
 
 
