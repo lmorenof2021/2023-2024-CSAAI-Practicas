4cc1f1d1282e1721bff220c68f278b392aea922b
l.morenof.2021 <l.morenof.2021@alumnos.urjc.es>
2024-04-08 12:35:10 +0200

    p3

--- P3/tiro.js
+++ P3/tiro.js
@@ -1,6 +1,17 @@
 console.log("Ejecutando JS...");
+const canvas= document.getElementById("ctiro");
+/* const platano = new Image();
+function init() {
+  platano.src = "platano.jpeg";
+  window.requestAnimationFrame(draw);
+} */
 
-const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//definir tamaño canvas
+canvas.width = 1800;
+canvas.height = 500;
+const display = document.getElementById("display")
 const range = document.getElementById("range");
 const range1 = document.getElementById("range1");
 const range_disp = document.getElementById("range_disp");
@@ -8,129 +19,127 @@ const range1_disp = document.getElementById("range1_disp");
 const disparar = document.getElementById("disparar");
 const iniciar = document.getElementById("iniciar");
 
-
-
-//-- Definir el tamaño del canvas
-canvas.width = 1800;
-canvas.height = 600;
-
-//-- Obtener el contexto del canvas
-const circulo = canvas.getContext("2d");
-const cuadrado = canvas.getContext("2d");
-
-
-const maxx = canvas.width - 50
-
-
-
-function getRandomInt(min,max) {
-return Math.floor(Math.random() * (max-min +1)+min);
+range.oninput = () => {
+  range_disp.innerHTML = range.value;
+  angulo = range.value;
 }
 
-x = getRandomInt(300,maxx);
- //(max - min) +  max
-
- function dibujarCirculo(x) {
-    circulo.beginPath();
-    //-- Dibujar un circulo: coordenadas x,y del centro
-    //-- Radio, Angulo inicial y angulo final
-
-    circulo.arc(x, 555, 40, 0, 2 * Math.PI);
-    circulo.strokeStyle = 'blue';
-    circulo.lineWidth = 3;
-    circulo.fillStyle = 'red';
-
-    //-- Dibujar el trazo
-    circulo.stroke()
-
-    //-- Dibujar el relleno
-    circulo.fill()
-    
-    
-    circulo.closePath()
- }
-
-dibujarCirculo(x);
-
-function dibujarCuadrado(x,y,lx,ly,color) {
-
-        cuadrado.beginPath();
-
-        //distancia x,y, ancho,alto
-        cuadrado.rect(x, y, lx, ly);
-
-        //-- Dibujar
-        cuadrado.fillStyle = color;
-        cuadrado.strokeStyle = 'blue';
-
-        //-- Rellenar
-        cuadrado.fill();
-
-        //-- Dibujar el trazo
-        cuadrado.stroke()
-        cuadrado.closePath();
-
+range1.oninput = () => {
+  range1_disp.innerHTML = range1.value;
+  velp = 0.1*range1.value;
 }
 
-//-- Coordenadas iniciales del proyectil
-let xop = 50;
-let yop = 530;
+
+function getRandomXO(min,max) {
+  return x = Math.floor(Math.random() * (max-min +1)+min);
+  }
+//-- Coordenadas iniciales del objetivo
+let xomin = 200;
+let xomax = 770;
+let xo = getRandomXO(xomin,xomax);
+let yo = 470;
+
+//--Coordenadas iniciales del proyectil
+let xop = 5;
+let yop = 445;
 let xp = xop;
 let yp = yop;
-dibujarCuadrado(xop, yop, 60, 60, "green");
+
+//-- función para pintar el proyectil
+function dibujarP(x,y,lx,ly,color) {
+
+  //-- Pintando el proyectil
+  ctx.beginPath();
+
+  //-- Definir un rectángulo de dimensiones lx x ly,
+  ctx.rect(x, y, lx, ly);
+
+  //-- Color de relleno del rectángulo
+  ctx.fillStyle = color;
+
+  //-- Mostrar el relleno
+  ctx.fill();
+
+  //-- Mostrar el trazo del rectángulo
+  ctx.stroke();
+
+  ctx.closePath();
+}
+//-- función para pintar el objetivo
+function dibujarO(x,y) {
+
+  //-- Pintando el objetivo
+  ctx.beginPath();
+
+  //-- Dibujar un circulo: coordenadas x,y del centro
+  //-- Radio, Angulo inicial y angulo final
+  ctx.arc(x, y, 25, 0, 2 * Math.PI);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 2;
+  ctx.fillStyle = 'red';
+
+  //-- Dibujar el relleno
+  ctx.fill()    
+
+  //-- Dibujar el trazo
+  ctx.stroke();
+
+  ctx.closePath();
+}
+
+//-- Dibujar el objetivo
+dibujarO(xo,yo); // Pintar el objetivo
+
+//-- Dibujar el proyectil
+dibujarP(xop, yop, 50, 50, "green"); // Pintar el proyectil
+
+//-- Velocidad del proyectil
+let t = 0;
 
 
-let velp = 5;
-
+//-- Función principal de actualización
 function lanzar() 
 {
   //-- Implementación del algoritmo de animación:
 
   //-- 1) Actualizar posición de los elementos
-  xp = xp + velp;
+  g = 0.1*9.8; //gravedad
+    
+  velx = velp*Math.cos((angulo*Math.PI)/180); //velocidad en el eje x
+  vely = velp*Math.sin((angulo*Math.PI)/180); //velocidad en el eje y
+
+  xp = xp + velx*t;
+  yp = yp - vely*t + 0.5*g*t*t;
+   
+  t += 0.1;
 
   //-- 2) Borrar el canvas
-  cuadrado.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   //-- 3) Pintar los elementos en el canvas
-  dibujarCuadrado(xp, yp, 60, 60, "blue"); // Pintar el proyectil
-  dibujarCirculo(x);
+  dibujarO(xo,yo); // Pintar el objetivo
+
+  dibujarP(xp, yp, 50, 50, "yellow"); // Pintar el proyectil
+
   //-- 4) Repetir
   requestAnimationFrame(lanzar);
 }
 
-
 //-- Función de retrollamada del botón de disparo
 disparar.onclick = () => {
+  crono.start()
   lanzar();
 }
 
-range.oninput = () => {
-    range_disp.innerHTML = range.value;
+iniciar.onclick = () => {
+  dibujarP(xop, yop, 50, 50, "blue");
+  //-recargamos el script
+  location.reload();
 }
-
-range1.oninput = () => {
-  range1_disp.innerHTML = range1.value;
-}
-
-
-
-
-
-
 
 
 //-- Definir un objeto cronómetro
 const crono = new Crono(display);
 
 
-//-- Arranque del cronometro
-// disparar.onclick = () => {
-//   console.log("Start!!");
-//   crono.start();
-// }
 
-//-- Función de retrollamada del botón iniciar
-iniciar.onclick = () => {
-  location.reload();
-}
